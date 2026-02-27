f9525aff52bc51959553483fe482597abd40cb3e
Banking simulations <ladimp11@gmail.com>
2026-02-28 00:24:44 +0300

    Initialize database tables on app startup
    Added database engine and created all tables.

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from app.routes import auth
 
 app.include_router(auth.router)
 from app.models import user, account
+from app.database import engine
+from app.models import user, account
+
+Base.metadata.create_all(bind=engine)
