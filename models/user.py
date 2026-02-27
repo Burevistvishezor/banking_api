4a6510a09251d575e5f5048dca5cc61325e131dd
Banking simulations <ladimp11@gmail.com>
2026-02-28 00:22:56 +0300

    Define User model with attributes and relationship
    Added User class with attributes and relationship.

--- models/user.py
+++ models/user.py
@@ -1,2 +1,16 @@
 from sqlalchemy.orm import relationship
 accounts = relationship("Account", back_populates="owner")
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    accounts = relationship("Account", back_populates="owner")
+  
